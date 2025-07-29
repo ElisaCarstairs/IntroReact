8586c851d8fb69211f30cb737565d18196dcba8d
ElisaCarstairs <marina.arredondo.h@gmail.com>
2025-07-28 23:03:47 -0600

    Clase2

---
+++ Clase2.1/estado_manejo/src/Componentes/Header.jsx
@@ -0,0 +1,12 @@
+
+export default function Header () {
+
+    return (
+        <header>
+            <h1>Bienvenido al Sitio</h1>
+            <h2>Lista de Compras</h2>
+        </header>
+    )
+
+}
+
